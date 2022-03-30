b689ff834fbf2b74de3e6f155f2417858262cdb3
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-03-29 22:26:00 -0300

    create DefenderReport.ps1

---
+++ DefenderReport.ps1
@@ -0,0 +1,15 @@
+﻿$txt = ".\DefenderReport.txt"
+$content = 
+@"
+EQUIPO/USUARIO: $(whoami)
+!!!Actualizando base de datos de virus: $(Update-MpSignature)
+Realizando escaneo completo del equipo: $(Start-MpScan -ScanType FullScan)
+Fecha de inicio del último scan: $(Get-MpComputerStatus | Select-Object -ExpandProperty FullScanStartTime)
+Fecha de finalización del último scan: $(Get-MpComputerStatus | Select-Object -ExpandProperty FullScanEndTime)
+Detalle de las amenazas encontradas:
+$(Get-MpThreatDetection)
+Removiendo amenazas
+$(Remove-MpThreat)
+"@
+
+Add-Content -LiteralPath $txt -Value $content
