93a9651d8618315d2cec6623ee0588e8d06e1d45
cyphersprojekt <89570332+cyphersprojekt@users.noreply.github.com>
2022-06-27 09:41:17 -0300

    Update DefenderReport.ps1
    Agrega la configuración de red a los resultados

--- DefenderReport.ps1
+++ DefenderReport.ps1
@@ -1,13 +1,28 @@
-﻿$txt = ".\DefenderReport.txt"
+$txt = ".\DefenderReport.txt"
+$config = (Get-NetIPConfiguration -Detailed)
 $content = 
 @"
-EQUIPO/USUARIO: $(whoami)
-!!!Actualizando base de datos de virus: $(Update-MpSignature)
-Realizando escaneo completo del equipo: $(Start-MpScan -ScanType FullScan)
-Fecha de inicio del último scan: $(Get-MpComputerStatus | Select-Object -ExpandProperty FullScanStartTime)
-Fecha de finalización del último scan: $(Get-MpComputerStatus | Select-Object -ExpandProperty FullScanEndTime)
+EQUIPO/USUARIO: 
+$(whoami)
+
+CONFIGURACIÓN DE RED:
+$(foreach($item in $config){$item | Select-Object -Property * | Out-String})
+
+Actualizando base de datos de virus
+$(Update-MpSignature)
+
+Realizando escaneo completo del equipo 
+$(Start-MpScan -ScanType FullScan)
+
+Fecha de inicio del último scan: 
+$(Get-MpComputerStatus | Select-Object -ExpandProperty FullScanStartTime)
+
+Fecha de finalización del último scan: 
+$(Get-MpComputerStatus | Select-Object -ExpandProperty FullScanEndTime)
+
 Detalle de las amenazas encontradas:
 $(Get-MpThreatDetection)
+
 Removiendo amenazas
 $(Remove-MpThreat)
 "@
